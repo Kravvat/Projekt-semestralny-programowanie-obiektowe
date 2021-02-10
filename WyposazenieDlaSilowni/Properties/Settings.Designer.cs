9d9556c55e67b7a196f4ec1e8bd131077e24cb86
Kacper Rafał Stec <steckacper@wp.pl>
2021-02-10 17:40:21 +0100

    Connect Database to C# Project
    Zaktualizowanie Data Connections oraz Data Sources w obrebie projektu

--- WyposazenieDlaSilowni/Properties/Settings.Designer.cs
+++ WyposazenieDlaSilowni/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WyposazenieDlaSilowni.Properties
-{
-
-
+namespace WyposazenieDlaSilowni.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=BAZOOKA\\SQLEXPRESS;Initial Catalog=\"Wyposazenie dla silowni\";Integrat" +
+            "ed Security=True")]
+        public string Wyposazenie_dla_silowniConnectionString {
+            get {
+                return ((string)(this["Wyposazenie_dla_silowniConnectionString"]));
             }
         }
     }
